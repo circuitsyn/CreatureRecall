acf2ffed3a69dafad0312c2d9ad522f65da7e545
CircuitSyn <circuitsyn@gmail.com>
2019-01-07 18:28:20 -0800

    added shuffle function

--- src/components/CardComponents/CardComponents.js
+++ src/components/CardComponents/CardComponents.js
@@ -2,10 +2,22 @@ import React from "react";
 import Cards from "../Card/card";
 import creaturesData from "../../creatures.json";
 
+//function to shuffle data
+function shuffleData(creaturesData) {
+    creaturesData.sort(function(){return 0.5 - Math.random()}); 
+    return creaturesData;
+}
+
+
 
 function CardComponents() {
-        const creatureComponents = creaturesData.map(creature =>
-            <Cards key={creature.id} name={creature.name} image={creature.image} class={creature.class} fact={creature.fact} link={creature.link} />)
+
+    //function call to shuffle data
+    shuffleData(creaturesData);
+
+    //props build to map data to different cards
+    let creatureComponents = creaturesData.map(creature =>
+        <Cards key={creature.id} name={creature.name} image={creature.image} class={creature.class} fact={creature.fact} link={creature.link} />)
     
 
     return (
